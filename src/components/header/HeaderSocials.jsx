a408758983a04a1bd521ba44a23060ac7dd63377
AHMEDAGNAOU <72224528+AHMEDAGNAOU@users.noreply.github.com>
2023-12-25 17:17:12 +0100

    cmd changes

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import { FiDribbble } from "react-icons/fi";
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='#linkdin' target='_blank'><BsLinkedin /></a>
-        <a href='#github' target='_blank'><FaGithub /></a>
-        <a href='#' target='_blank'><FiDribbble /></a>
+        <a href='https://www.linkedin.com/in/agnaou-agnaou-161a3428b/' target='_blank'><BsLinkedin /></a>
+        <a href='https://github.com/AHMEDAGNAOU/Portfolio' target='_blank'><FaGithub /></a>
+        <a href='https://dribbble.com/' target='_blank'><FiDribbble /></a>
     </div>
   )
 }
